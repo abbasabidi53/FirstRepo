7eaae2aa0058f142dfe7c510bf17b2413a35768f
abbasabidi53 <abbas53@hotmail.com>
2019-10-02 20:07:33 -0700

    myfirstcommit

---
+++ Learning/src/Student.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("please enter date");
+
+			while{(true){
+			
+			String date = scanner.next();
+			//System.out.print(date);
+
+			//String some String = "";
+
+			String[]words = date.split("/");
+
+			
+		if (words[0]=="1" || words[0]== "2"|| words[0]== "3"|| words[0]== "4"|| words[0]== "5"|| words[0]== "6"){
+
+		
+		break:
+			
+		else
+			System.out.println("incorrect Month");
+
+			continue;
+		}	
+			System.out.println(" "+ words[0]);
+			System.out.println(""+ words[1]);
+			System.out.println(" "+ words[2]);
+			
+			String[]words = date.split("/");
+
+
+			switch(words[0]){
+				
+			 case "1":
+					System.out.println("January");
+				break;	
+			 case "01":
+					System.out.println("January");
+				break;	
+				case "2":
+					System.out.println("February");
+				break;
+				case "02":
+					System.out.println("Feburary");
+				break;	
+				case "3":
+					System.out.println("March");
+				break;
+				case "03":
+					System.out.println("March");
+				break;
+				case "4":
+					System.out.println("April");
+				break;	
+				case "04":
+					System.out.println("April");
+				break;	
+				case "5":
+					System.out.println("May");
+				break;
+				case "05":
+					System.out.println("May");
+				break;
+				case "6":
+					System.out.println("June");
+				break;
+				case "06":
+					System.out.println("June");
+				break;
+				case "7":
+					System.out.println("July");
+				break;	
+				case "07":
+					System.out.println("July");
+				break;	
+				case "8":
+					System.out.println("August");
+				break;
+				case "08":
+					System.out.println("August");
+				break;
+				case "9":
+					System.out.println("September");
+				break;
+				case "09":
+					System.out.println("September");
+				break;
+				case "10":
+					System.out.println("October");
+				break;	
+				case "010":
+					System.out.println("October");
+				break;	
+				case "11":
+					System.out.println("November");
+				break;
+				case "011":
+					System.out.println("November");
+				break;
+				case "12":
+					System.out.println("December");
+				break;
+				case "012":
+					System.out.println("December");
+				break;
+				
+				
+				default:
+					
+					
+				if{
+					switch(world[1])= (003);
+					
+							System.out.println("invalidDate");
+				}
+				
+				
+			}
+			
+			
+			
+			
+			 
+ 
+
+
+
+
